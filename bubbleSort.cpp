35111aeb56f62ecaf83dedde0e3587296a6641a2
Rishit <rishitbutola10@gmail.com>
2025-01-13 10:18:27 +0530

    bubblesort

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -1,3 +1,50 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
+void exchange(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void bubbleSort(int arr[], int n)
+{ // Removed trailing comma
+    for (int i = 0; i < n - 1; i++)
+    { // Outer loop for passes
+        for (int j = 0; j < n - i - 1; j++)
+        { // Inner loop for comparisons
+            if (arr[j] > arr[j + 1])
+            {
+                exchange(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n]; // Dynamically sized array
+    cout << "Enter " << n << " integers: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    // Sort the array using the bubbleSort function
+    bubbleSort(arr, n);
+
+    // Display the sorted array
+    cout << "Sorted array in ascending order: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
